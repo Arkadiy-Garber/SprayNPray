abdf96ebfe1a3da0f64d258f37e0d6ddb0785045
Arkadiy Garber <arkadiygarber@wl-180-67.wireless.umt.edu>
2019-10-19 17:15:21 -0600

    setup file

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+chmod +x BinBlaster.py
+
 printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
 
 ## adding conda channels
@@ -14,7 +16,7 @@ conda config --add channels conda-forge 2> /dev/null
 conda config --add channels au-eoed 2> /dev/null
 
 ## creating GToTree environment and installing dependencies
-conda create -n parahunt diamond prodigal metabat2 --yes
+conda create -n binblast diamond prodigal metabat2 --yes
 
 ## activating environment
 source activate binblast
@@ -28,7 +30,7 @@ echo '#!/bin/sh'" \
 export ctl=\"$(pwd)/codeml-2.ctl\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
 
 # re-activating environment so variable and PATH changes take effect
-source activate parahunt
+source activate binblast
 
 
 printf "\n        ${GREEN}DONE!${NC}\n\n"
