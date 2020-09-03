69f83766eefca2de59abf2ce960cc614b2b4d5c5
Arkadiy-Garber <arkg@udel.edu>
2020-09-03 18:40:11 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-chmod +x BinBlaster.py
+chmod +x spray-and-pray.py
 
 printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
 
@@ -16,10 +16,10 @@ conda config --add channels conda-forge 2> /dev/null
 conda config --add channels au-eoed 2> /dev/null
 
 ## creating GToTree environment and installing dependencies
-conda create -n binblast diamond prodigal metabat2 --yes
+conda create -n sprayandpray diamond prodigal metabat2 --yes
 
 ## activating environment
-source activate binblast
+source activate sprayandpray
 
 ## creating directory for conda-env-specific source files
 mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
@@ -33,7 +33,7 @@ echo '#!/bin/sh'" \
 export ctl=\"$(pwd)/codeml-2.ctl\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
 
 # re-activating environment so variable and PATH changes take effect
-source activate binblast
+source activate sprayandpray
 
 
 printf "\n        ${GREEN}DONE!${NC}\n\n"
