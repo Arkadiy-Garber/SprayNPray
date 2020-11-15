861e31a89622e17fbbe8764f53e5b8dc9fc52bd0
Arkadiy-Garber <arkg@udel.edu>
2020-11-14 22:16:51 -0700

    Add files via upload

--- spray-and-pray.py
+++ spray-and-pray.py
@@ -388,70 +388,106 @@ parser = argparse.ArgumentParser(
     '''))
 
 parser.add_argument('-g', type=str, help="Input bin/assembly in FASTA format", default="NA")
+
 parser.add_argument('-o', type=str, help="Input ORFs in FASTA amino acid format", default="NA")
+
 parser.add_argument('-ref', type=str, help="Input reference protein database (recommended: nr). Could be FASTA file or "
                                            "DIAMOND database file (with extension .dmnd)", default="NA")
+
 parser.add_argument('-bam', type=str, help="Input sorted BAM file with coverage info (optional)", default="NA")
+
 parser.add_argument('-out', type=str, help="Name output file", default="NA")
+
 parser.add_argument('-t', type=int, help="number of threads to use for DIAMOND BLAST", default=1)
-parser.add_argument('--skip_makedb', type=str, help="if the DIAMOND database already exists "
-                                                    "(i.e. file with extension .dmnd), and you provided that file to "
-                                                    "the -ref argument, then include this flag to skip the time-consuming "
-                                                    "step of building the DIAMOND BLAST database", const=True, nargs="?")
-parser.add_argument('--spades', type=str, help="is this a SPAdes assembly, with the SPAdes headers? If so, "
+
+parser.add_argument('--makedb', type=str, help="if the DIAMOND database does not already exist "
+                                                    "(i.e. file with extension .dmnd), and you would like the program t"
+                                               "o run  diamond makedb, provide this flag", const=True, nargs="?")
+
+parser.add_argument('--spades', type=str, help="is this a SPAdes assembly, with the original SPAdes headers? If so, "
                                                "then you can provide this flag, and BinBlaster will summarize using the coverage "
                                                "information provided in the SPAdes headers", const=True, nargs="?")
-parser.add_argument('--meta', type=str, help="meta procedure for prodigal", const=True, nargs="?")
+
+parser.add_argument('--meta', type=str, help="contigs are from a mixed community of organisms", const=True, nargs="?")
+
+
+parser.add_argument('--fa', type=str, help="write subset of contigs that match user-specified parameters to a separate FASTA file", const=True, nargs="?")
+
+parser.add_argument('-blast', type=str, help="DIAMOND BLAST output file from previous run", default="NA")
+
+parser.add_argument('-genus', type=str, help="genus name expected among hits to provided contigs, to be written to FASTA file", default="NA")
+
+parser.add_argument('-species', type=str, help="species name expected among hits to provided contigs, to be written to FASTA file (provide only if you also provided the genus name)", default="NA")
+
+parser.add_argument('-perc', type=float, help="percentage of total hits to the contig that must be to the specified genus/species for writing to FASTA", default=0)
+
+parser.add_argument('-gc', type=float, help="minimum GC-content of contigs to write to FASTA (default = 0)", default=0)
+
+parser.add_argument('-GC', type=float, help="maximum GC-content of contigs to write to FASTA (default = 100)", default=100)
+
+parser.add_argument('-cov', type=float, help="minimum coverage of contigs to write to FASTA (default = 0)", default=0)
+
+parser.add_argument('-COV', type=float, help="maximum coverage of contigs to write to FASTA (default = 100000000)", default=100000000)
+
+parser.add_argument('-cd', type=float, help="minimum coding density (in hits/kb) to write to FASTA (default = 0.25)", default=0.25)
+
+parser.add_argument('-CD', type=float, help="maximum coding density (in hits/kb) to write to FASTA (default = 5)", default=5)
+
+parser.add_argument('-l', type=float, help="minimum length of contig to write to FASTA (default = 1000)", default=1000)
+
+parser.add_argument('-L', type=float, help="maximum length of contig to write to FASTA (default = 100000000)", default=100000000)
+
 
 args = parser.parse_args()
 
 if args.o != "NA":
     file = open(args.o)
     file = fasta2(file)
-    if not args.skip_makedb:
-        print("Running Diamond: making DIAMOND BLAST database")
+    if args.makedb:
+        print("Running DIAMOND: making DIAMOND database")
         os.system("diamond makedb --in %s --db %s.dmnd" % (args.ref, args.ref))
 
-        print("Running Diamond: running DIAMOND BLAST")
-        os.system("diamond blastp --db %s.dmnd --query %s --outfmt 6 --out %s.blast --max-target-seqs 15 --evalue 1E-6 --threads %d" % (args.ref, args.o, args.o, args.t))
+    print("Running DIAMOND BLAST")
+    os.system(
+        "diamond blastp --db %s.dmnd --query %s-proteins.faa "
+            "--outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore stitle "
+            "--out %s.blast --max-target-seqs 1 --evalue 1E-6 --threads %d --query-cover 50"
+        % (args.ref, args.o, args.o, args.t))
 
-    else:
-        print("Running Diamond: skipping makedb and running DIAMOND BLAST")
-        os.system(
-            "diamond blastp --db %s.dmnd --query %s --outfmt 6 --out %s.blast --max-target-seqs 15 --evalue 1E-6 --threads %d"
-            % (args.ref, args.o, args.o, args.t))
-
-    print("extracting DIAMOND BLAST hit information")
-    os.system("blast-to-fasta.sh %s.blast %s %s.blast-fasta" % (args.o, args.ref, args.o))
-
-    print("cutting...")
-    os.system("cut -f2 %s.blast > ids.txt" % (args.o))
-    print("running seqtk...")
-    os.system("seqtk subseq %s ids.txt > %s.blast-fasta" % (args.ref, args.o))
-    os.system("rm ids.txt")
+    # print("extracting DIAMOND BLAST hit information")
+    # os.system("blast-to-fasta.sh %s.blast %s %s.blast-fasta" % (args.o, args.ref, args.o))
+    #
+    # print("cutting...")
+    # os.system("cut -f2 %s.blast > ids.txt" % (args.o))
+    # print("running seqtk...")
+    # os.system("seqtk subseq %s ids.txt > %s.blast-fasta" % (args.ref, args.o))
+    # os.system("rm ids.txt")
 
     print("Preparing summary: %s" % args.out)
-    nameDict = defaultdict(lambda: defaultdict(lambda: 'EMPTY'))
-    blastFasta = open("%s.blast-fasta" % args.o)
-    for i in blastFasta:
-        if re.match(r'>', i):
-            line = (i.rstrip()[1:])
-            ls = line.split(" ")
-            id = (ls[0])
-            try:
-                name = (allButTheFirst(line[0:150], " "))
-                name = name.split("]")[0]
-                name = name.split("[")[1]
-                nameDict[id] = name
-            except IndexError:
-                pass
+    # nameDict = defaultdict(lambda: defaultdict(lambda: 'EMPTY'))
+    # blastFasta = open("%s.blast-fasta" % args.o)
+    # for i in blastFasta:
+    #     if re.match(r'>', i):
+    #         line = (i.rstrip()[1:])
+    #         ls = line.split(" ")
+    #         id = (ls[0])
+    #         try:
+    #             name = (allButTheFirst(line[0:150], " "))
+    #             name = name.split("]")[0]
+    #             name = name.split("[")[1]
+    #             nameDict[id] = name
+    #         except IndexError:
+    #             pass
+
     aaiDict = defaultdict(list)
     blastDict = defaultdict(list)
     blast = open("%s.blast" % args.o)
     for i in blast:
         ls = i.rstrip().split("\t")
         orf = ls[0]
-        name = (nameDict[ls[1]])
+        name = (ls[12])
+        name = name.split("]")[0]
+        name = name.split("[")[1]
         blastDict[orf].append(name)
         aai = ls[2]
         aaiDict[orf].append(float(aai))
@@ -480,11 +516,13 @@ if args.o != "NA":
 
 
 else:
+
     file = open(args.g)
     file = fasta2(file)
     total = 0
     for i in file.keys():
         total += len(file[i])
+
 
     if total < 20000:
         if args.meta:
@@ -493,34 +531,39 @@ else:
             print("looks like there are less than 20000 characters in your provided sequences file. Please re-run the script with the --meta flag")
             raise SystemExit
 
-    print("Running Prodigal: calling ORFs from provided contigs")
-    if args.meta:
-        os.system("prodigal -i %s -a %s-proteins.faa -p meta" % (args.g, args.g))
-    else:
-        os.system("prodigal -i %s -a %s-proteins.faa" % (args.g, args.g))
+    if args.blast == "NA":
 
-    if not args.skip_makedb:
-        print("Running Diamond: making DIAMOND BLAST database")
-        os.system("diamond makedb --in %s --db %s.dmnd" % (args.ref, args.ref))
+        print("Running Prodigal: calling ORFs from provided contigs")
+        if args.meta:
+            os.system("prodigal -i %s -a %s-proteins.faa -p meta" % (args.g, args.g))
+        else:
+            os.system("prodigal -i %s -a %s-proteins.faa" % (args.g, args.g))
 
-        print("Running Diamond: running DIAMOND BLAST")
-        os.system("diamond blastp --db %s.dmnd --query %s-proteins.faa --outfmt 6 --out %s.blast --max-target-seqs 1 --evalue 1E-6 --threads %d" % (args.ref, args.g, args.g, args.t))
+        if args.makedb:
+            print("Running Diamond: making DIAMOND BLAST database")
+            os.system("diamond makedb --in %s --db %s.dmnd" % (args.ref, args.ref))
 
-    else:
-        print("Running Diamond: skipping makedb and running DIAMOND BLAST")
+
+        print("Running Diamond BLAST")
         os.system(
-            "diamond blastp --db %s.dmnd --query %s-proteins.faa --outfmt 6 --out %s.blast --max-target-seqs 1 --evalue 1E-6 --threads %d"
+            "diamond blastp --db %s.dmnd --query %s-proteins.faa "
+            "--outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore stitle "
+            "--out %s.blast --max-target-seqs 1 --evalue 1E-6 --threads %d --query-cover 50"
             % (args.ref, args.g, args.g, args.t))
 
-    print("extracting DIAMOND BLAST hit information")
-    os.system("blast-to-fasta.sh %s.blast %s %s.blast-fasta" % (args.g, args.ref, args.g))
+        blastFile = "%s.blast" % args.g
+    else:
+        blastFile = args.blast
 
-
-    print("cutting...")
-    os.system("cut -f2 %s.blast > ids.txt" % (args.g))
-    print("running seqtk...")
-    os.system("seqtk subseq %s ids.txt > %s.blast-fasta" % (args.ref, args.g))
-    os.system("rm ids.txt")
+    # print("extracting DIAMOND BLAST hit information")
+    # os.system("blast-to-fasta.sh %s.blast %s %s.blast-fasta" % (args.g, args.ref, args.g))
+    #
+    #
+    # print("cutting...")
+    # os.system("cut -f2 %s.blast > ids.txt" % (args.g))
+    # print("running seqtk...")
+    # os.system("seqtk subseq %s ids.txt > %s.blast-fasta" % (args.ref, args.g))
+    # os.system("rm ids.txt")
 
     if args.bam != "NA":
         print("Extracting coverage information from the provided BAM files")
@@ -530,42 +573,44 @@ else:
     gcDict = defaultdict(lambda: defaultdict(lambda: 'EMPTY'))
     GC = 0
     total = 0
-    assembly = open(args.g)
-    assembly = fasta2(assembly)
-    for i in assembly.keys():
-        seq = assembly[i]
+    for i in file.keys():
+        seq = file[i]
         total += len(seq)
         gc = 0
         for bp in seq:
             if bp == "C" or bp == "G":
                 GC += 1
                 gc += 1
-        gcDict[i] = str(float(gc/len(seq)))
+        gcDict[i] = str( float(gc/len(seq)) * 100 )
 
     print("Preparing summary: %s" % args.out)
-    nameDict = defaultdict(lambda: defaultdict(lambda: 'EMPTY'))
-    blastFasta = open("%s.blast-fasta" % args.g)
-    for i in blastFasta:
-        if re.match(r'>', i):
-            line = (i.rstrip()[1:])
-            ls = line.split("\t")
-            id = (ls[0])
-            try:
-                name = (allButTheFirst(line[0:150], " "))
-                name = name.split("]")[0]
-                name = name.split("[")[1]
-                nameDict[id] = name
-            except IndexError:
-                pass
-
+    # nameDict = defaultdict(lambda: defaultdict(lambda: 'EMPTY'))
+    # blastFasta = open("%s.blast-fasta" % args.g)
+    # for i in blastFasta:
+    #     if re.match(r'>', i):
+    #         line = (i.rstrip()[1:])
+    #         ls = line.split("\t")
+    #         id = (ls[0])
+    #         try:
+    #             name = (allButTheFirst(line[0:150], " "))
+    #             name = name.split("]")[0]
+    #             name = name.split("[")[1]
+    #             nameDict[id] = name
+    #         except IndexError:
+    #             pass
 
     aaiDict = defaultdict(list)
     blastDict = defaultdict(list)
-    blast = open("%s.blast" % args.g)
+    blast = open(blastFile)
     for i in blast:
         ls = i.rstrip().split("\t")
         contig = allButTheLast(ls[0], "_")
-        name = (nameDict[ls[1]])
+        name = ls[12]
+        try:
+            name = name.split("]")[0]
+            name = name.split("[")[1]
+        except IndexError:
+            name = "NA"
         aai = ls[2]
         blastDict[contig].append(name)
         aaiDict[contig].append(float(aai))
@@ -578,32 +623,79 @@ else:
             if ls[1] != "contigLen":
                 depthDict[ls[0]]["length"] = int(ls[1])
                 depthDict[ls[0]]["depth"] = ls[2]
+    
 
     out = open(args.out, "w")
     out.write("contig" + "," + "contig_length" + "," + "hits_per_contig" + "," + "cov" + "," + "GC-content" + "," + "Average_AAI" + "," + "closest_blast_hits" + "\n")
-    for i in assembly.keys():
+    for i in file.keys():
         if args.bam != "NA":
             depth = depthDict[i]["depth"]
             length = depthDict[i]["length"]
         elif args.spades:
             depth = lastItem(i.split("_"))
-            length = len(assembly[i])
+            length = len(file[i])
         else:
             depth = "Unknown"
-            length = len(assembly[i])
+            length = len(file[i])
         gc = gcDict[i]
         hitsList = blastDict[i]
         try:
             AAI = statistics.mean(aaiDict[i])
         except statistics.StatisticsError:
             AAI = "NA"
-        out.write(i + "," + str(length) + "," + str(len(hitsList)/length) + "," + str(depth) + "," + str(gc) + "," + str(AAI) + ",")
+        out.write(i + "," + str(length) + "," + str(len(hitsList) / (length / 1000)) + "," + str(depth) + "," + str(gc) + "," + str(AAI) + ",")
         for j in hitsList:
             try:
                 out.write(j + "; ")
             except TypeError:
                 pass
         out.write("\n")
+    out.close()
+
+
+    if args.fa:
+        summary = open(args.out)
+        out = open(args.out + '-contigs.fa', "w")
+
+        for i in summary:
+            ls = i.rstrip().split(",")
+            if ls[1] != "contig_length":
+                length = float(ls[1])
+                hitsperkb = float(ls[2])
+                gc = float(ls[4])
+
+                if ls[3] != "Unknown":
+                    cov = float(ls[3])
+                else:
+                    cov = 0
+
+                perc = 100
+                if args.genus != "NA":
+                    genus = args.genus
+                    hits = ls[6].split("; ")
+                    totalHits = len(hits)
+                    matches = 0
+
+                    for j in hits:
+                        if j.split(" ")[0] == genus:
+
+                            if args.species != "NA":
+                                species = args.species
+                                if j.split(" ")[1] == species:
+                                    matches += 1
+                            else:
+                                matches += 1
+
+                    perc = (matches/totalHits)*100
+
+                if perc >= args.perc and gc >= args.gc and gc <= args.GC and length >= args.l and length <= args.L and cov >= args.cov and cov <= args.COV:
+                    out.write(">" + ls[0] + "\n")
+                    out.write(file[ls[0]] + "\n")
+
+        out.close()
+
+    else:
+        pass
 
     print("Finished!")
 
