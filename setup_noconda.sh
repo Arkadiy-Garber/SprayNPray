1eb9af9314efe09411da29b15e6101b30fbbab21
Arkadiy-Garber <agarber4@asu.edu>
2021-03-25 13:12:26 -0700

    Create setup_noconda.sh

---
+++ setup_noconda.sh
@@ -0,0 +1,5 @@
+Rscript -e 'install.packages("tm", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("SnowballC", repos = "http://cran.us.r-project.org”)'
+Rscript -e 'install.packages("RColorBrewer", repos = "http://cran.us.r-project.org”)'
+Rscript -e 'install.packages("XML", repos = "http://cran.us.r-project.org")'
+Rscript -e 'install.packages("argparse", repos = "http://cran.us.r-project.org")'
